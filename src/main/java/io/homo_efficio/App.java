972766918c4a1b034dc295053ab427b671db3438
Homo Efficio <homo.efficio@gmail.com>
2020-07-20 15:10:46 +0900

    maven-fat-jar 2가지 방법 예제 추가

---
+++ src/main/java/io/homo_efficio/App.java
@@ -0,0 +1,15 @@
+package io.homo_efficio;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author homo.efficio@gmail.com
+ * created on 2020-07-20
+ */
+@Slf4j
+public class App {
+
+    public static void main(String[] args) {
+        log.info("maven으로 만든 fat-jar에서 logback + slf4j 정상 동작함");
+    }
+}
