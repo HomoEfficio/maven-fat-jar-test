f91457bf33e462cd13f2e0a3ea3204ee68523c16
Homo Efficio <homo.efficio@gmail.com>
2020-07-21 11:29:41 +0900

    getResource().openStream() 예제 추가

--- src/main/java/io/homo_efficio/App.java
+++ src/main/java/io/homo_efficio/App.java
@@ -15,6 +15,8 @@ public class App {
     public static void main(String[] args) throws IOException {
         ResourceLoader resourceLoader =
                 new ResourceLoader("/static", Path.of("/static"));
+
         resourceLoader.loadResourceAsFile("/folder1/sample1");
+        resourceLoader.loadResourceAsStream("/folder1/sample1");
     }
 }
--- src/main/java/io/homo_efficio/ResourceLoader.java
+++ src/main/java/io/homo_efficio/ResourceLoader.java
@@ -6,6 +6,9 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 
 /**
@@ -25,14 +28,32 @@ public class ResourceLoader {
         log.info("content root: {}", rootPath);
         log.info("resourceLocation: {}", resourceLocation);
 
-        String fileLocation = this.getClass().getResource(root + resourceLocation).getFile();
-        log.info("fileLocation from URL: {}", fileLocation);
+        URL resourceURL = this.getClass().getResource(root + resourceLocation);
+        log.info("resourceURL: {}", resourceURL);
 
-        File file = new File(fileLocation);
-        FileReader fileReader = new FileReader(file);
-        char[] chars = new char[(int) file.length()];
-        fileReader.read(chars);
+//        String fileLocation = resourceURL.getFile();
+//        log.info("fileLocation from URL: {}", fileLocation);
+//
+//        File file = new File(fileLocation);
+//        FileReader fileReader = new FileReader(file);
+//        char[] chars = new char[(int) file.length()];
+//        fileReader.read(chars);
+//
+//        log.info("resource contents: {}", new String(chars));
 
-        log.info("resource contents: {}", new String(chars));
+        URL resource = this.getClass().getResource(root + resourceLocation);
+        InputStream inputStream = resource.openStream();
+        byte[] bytes = inputStream.readAllBytes();
+        log.info("resource contents: {}", new String(bytes, StandardCharsets.UTF_8));
+    }
+
+    public void loadResourceAsStream(String resourceLocation) throws IOException {
+        log.info("OOO getResourceAsStream() 방식");
+        log.info("content root: {}", root);
+        log.info("resourceLocation: {}", resourceLocation);
+
+        InputStream resourceAsStream = this.getClass().getResourceAsStream(root + resourceLocation);
+        byte[] bytes = resourceAsStream.readAllBytes();
+        log.info("resource contents: {}", new String(bytes, StandardCharsets.UTF_8));
     }
 }
